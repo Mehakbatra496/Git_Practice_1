f8d0dc0705ba7ec26d779041bb1ab10e320dd5bc
Mehak <mehakatra496@gmail.com>
2024-04-17 12:19:16 +0530

    Added by Dev1

---
+++ dev1/Third3.java
@@ -0,0 +1,9 @@
+class Abc
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is first class made into Programmimg practice folder maually by me");
+        System.out.println("hlo modified 3");
+
+    }
+}
